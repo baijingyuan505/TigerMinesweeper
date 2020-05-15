ddf37c7c1f93b868f0d79524a9a06d2a1ec66bcf
hhz2045 <65063544+hhz2045@users.noreply.github.com>
2020-05-15 12:17:29 +0800

    Add files via upload

--- mines.sh
+++ mines.sh
@@ -25,6 +25,15 @@ BBLUE=44
 BPURPLE=45
 BSBLUE=46
 BGREY=47
+#new variables below
+#Index shows if this cell contains a mine
+#COUNT shows the mine number in 3*3 area
+#Times means how many times you can clear a cell
+Index=0
+COUNT=2
+Times=5
+#temporary var,please delete it after finished function Mine
+counter=1
 #--------------函数--------------
 #初始化函数（无必要请不要更改）
 function Init ()
@@ -47,10 +56,11 @@ function GameExit ()
         exit $OK
 }
 
-#游戏帮助：负责输入选择的同学请在这修改相关的指令
+#游戏帮助
 function Help ()
 {
-        msg="移动: 挖雷: 新游戏: 退出:"
+
+        msg="移动: W,A,S,D 挖雷: -J-open a cell,-K-clear a cell 新游戏:  N 退出: X "
         $ECHO "${ESC}${REV};${RED}m${ESC}24;1H${msg}${ESC}${NULL}m"
         return $OK
 }
@@ -107,7 +117,6 @@ function Draw ()
         do
                 $ECHO "${ESC}$((i+2));1H${line2} |"
         done
-        $ECHO "${ESC}$((Y+2));1H♣${line1}♠"
         return $OK
 }
 
@@ -132,16 +141,131 @@ function Main ()
 {
         local key
         #按键设置 
+        $ECHO "${ESC}?25h"
+        Modify
+#locate in centre
         while read -s -n 1 key
         do
                 case $key in
+
                 [nN])        exec $(dirname $0)/$(basename $0);;
                 [xX])        GameExit;;
+                [aA])    left;;
+                [dD])    right;;
+                [wW])    up;;
+                [sS])    down;;
+                [jJ])    Open;;
+                [kK])    Clear;;
                 esac
         done
         return $OK
 }
+#relocate in the center
+function Modify ()
+{
+row=$X
+col=`expr $Y + 1`
+for((i=1;i<=((X/2));i++))
+do
+left
+done
+for((i=1;i<=$((Y/2+1));i++))
+do
+up
+done
+return $OK
+}
+#open a cell
+function Open ()
+{
+if Operated
+then return $OK
+fi
+Count $row $col
+if [ $Index = X ]
+then GameOver
+     return $OK
+fi
+$ECHO "${ESC}${ORANGE}m${COUNT} "
+#please use var Index and COUNT
+#in the function Count
+$ECHO "${ESC}2D"
+return $OK
+}
+#clear a mine using one opportunities
+function Clear ()
+{
+if Operated
+then return $OK
+fi
+Times=`expr $Times - 1`
+if [ $Times -eq 0 ]
+then GameOver
+     return $OK
+fi
+if Mine
+then $ECHO "${ESC}${ORANGE}mY "
+else $ECHO "${ESC}${ORANGE}mN "
+fi
+$ECHO "${ESC}2D"
+return $OK
+}
+#move functions
+function left ()
+{
+if [ $col -ne 1 ]
+then $ECHO "${ESC}4D"
+col=`expr $col - 1` 
+fi
+return $OK
+}
+function right ()
+{
+if [ $col -ne $Y ]
+then $ECHO "${ESC}4C"
+col=`expr $col + 1`
+fi
+return $OK
+}
+function up ()
+{
+if [ $row -ne 1 ]
+then $ECHO "${ESC}A"
+row=`expr $row - 1`
+fi
+return $OK
+}
+function down ()
+{
+if [ $row -ne $X ]
+then $ECHO "${ESC}B"
+row=`expr $row + 1`
+fi
+return $OK
+}
+#count the mines in 3*3 areas
+function Count ()
+{
+return $OK
+}
+#see if this cell has a mine
+function Mine ()
+{
+counter=`expr $counter + 1`
+#counter is temporary variable
+if ((counter%2==0))
+then return 0
+else return 1
+fi
+}
+#see if this cell is operated
+function Operated ()
+{
+return 1
+}
+
 Init
 Menu
 Draw
 Main
+
